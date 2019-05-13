89e1edca9b76e766fdc486342c958f605edc02d0
opqi <petr.dmitriev.96@gmail.com>
2019-05-13 23:56:17 +0300

    [upd] kmeans (some benchmarks added)

--- unsupervisedlearning/clustering/kmeans/kmeans_test.go
+++ unsupervisedlearning/clustering/kmeans/kmeans_test.go
@@ -7,16 +7,50 @@ import (
 	"github.com/bayesiangopher/bayesiangopher/core"
 )
 
-func TestKmeans(t *testing.T) {
-	// func BenchmarkKmeans(b *testing.B) {
-	r_train := core.CSVReader{Path: "train.csv"}
-	r_test := core.CSVReader{Path: "test.csv"}
-	// r := core.CSVReader{Path: "../../../datasets/the_iris_kmeans_dataset.csv"}
+//Speed of fitting model for Xclara dataset
+func BenchmarkXclaraFitKmeans(b *testing.B) {
+	r := core.CSVReader{Path: "../../../datasets/the_xclara_cluster.csv"}
+	train := r.Read(true)
+
+	k := 3
+	kmns := KMNS{}
+	kmns.Fit(train, k)
+}
+
+//Speed of fitting model for Iris dataset
+func BenchmarkIrisFitKmeans(b *testing.B) {
+	r := core.CSVReader{Path: "../../../datasets/the_train_randblobs_cluster.csv"}
+	train := r.Read(true)
+
+	k := 3
+	kmns := KMNS{}
+	kmns.Fit(train, k)
+}
+
+//Speed of fitting model and predicting labels for randomly generated blobs
+func BenchmarkRandBlobsFitPredictKmeans(b *testing.B) {
+	r_train := core.CSVReader{Path: "../../../datasets/the_train_randblobs_cluster.csv"}
+	r_test := core.CSVReader{Path: "../../../datasets/the_test_randblobs_cluster.csv"}
 	train := r_train.Read(true)
 	test := r_test.Read(true)
 
 	k := 3
-	kmns := KMNS{state_init: false}
+	kmns := KMNS{}
+	kmns.Fit(train, k)
+	fmt.Println(kmns.labels)
+	lbs := kmns.Predict(test)
+	fmt.Println(lbs)
+}
+
+//Fitting model and predicting labels for randomly generated blobs
+func TestRandBlobsFitPredictKmeans(t *testing.T) {
+	r_train := core.CSVReader{Path: "../../../datasets/the_train_randblobs_cluster.csv"}
+	r_test := core.CSVReader{Path: "../../../datasets/the_test_randblobs_cluster.csv"}
+	train := r_train.Read(true)
+	test := r_test.Read(true)
+
+	k := 3
+	kmns := KMNS{}
 	kmns.Fit(train, k)
 	fmt.Println(kmns.labels)
 	lbs := kmns.Predict(test)
