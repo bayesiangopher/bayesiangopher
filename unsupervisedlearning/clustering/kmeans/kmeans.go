f0cd9c3a4c7509c50cc2cea5324c59fccd5754ac
Petr Dmitriev <p.dmitriev@partner.samsung.com>
2019-05-13 19:58:18 +0300

    [upd] kmeans (without testcases)

--- unsupervisedlearning/clustering/kmeans/kmeans.go
+++ unsupervisedlearning/clustering/kmeans/kmeans.go
@@ -1,9 +1,8 @@
 package kmeans
 
 import (
-	"fmt"
-	"math"
 	"math/rand"
+	"time"
 
 	"gonum.org/v1/gonum/mat"
 
@@ -11,31 +10,26 @@ import (
 )
 
 type KMNS struct {
-	n_init    int
-	max_iter  int
-	tol       float64
-	centroids *mat.Dense
-	labels    *mat.VecDense
+	max_iter   int
+	tol        float64
+	centroids  *mat.Dense
+	labels     []int //*mat.VecDense
+	state_init bool  //Random (false) or determined (true) state for centroid initialization
 }
 
 func (km *KMNS) Fit(train core.Train, k int) (err error) {
 	//Prepare data:
 	x_train := core.MakeMatrixFromTrain(train)
 	//Set optional parameters
-	if km.n_init == 0 {
-		km.n_init = 10
-	}
 	if km.max_iter == 0 {
-		km.max_iter = 10
+		km.max_iter = 300
 	}
-	if km.tol == 0 {
-		km.tol = 0.0001
+	if !km.state_init {
+		rand.Seed(time.Now().UnixNano())
 	}
-	//TODO: for n_init here
+	//Getting the best
 	iter := 0
 	n_instance, n_features := x_train.Dims()
-	km.labels = mat.NewVecDense(n_instance, nil)
-	fmt.Println(km.labels.Dims())
 	//init centroids
 	var ctrs []float64
 	for i := 0; i < k; i++ {
@@ -46,185 +40,69 @@ func (km *KMNS) Fit(train core.Train, k int) (err error) {
 	}
 	km.centroids = mat.NewDense(k, n_features, ctrs)
 	prev_ctrs := mat.NewDense(k, n_features, nil)
-	norm := norm_vec(km.centroids, prev_ctrs)
-	fmt.Println(norm)
+	norm := normDense(km.centroids, prev_ctrs)
 
-	for norm > km.tol {
+	for norm != km.tol {
 		if iter++; iter > km.max_iter {
 			break
 		}
-
+		prev_ctrs.Copy(km.centroids)
+		//assign all points in ds to centroids
+		km.labels = make([]int, n_instance)
+		for i := 0; i < n_instance; i++ {
+			dist := make([]float64, k)
+			for j := 0; j < k; j++ {
+				dist[j] = core.VecEuclidean(mat.VecDenseCopyOf(km.centroids.RowView(j)), mat.VecDenseCopyOf(x_train.RowView(i)))
+			}
+			km.labels[i] = minElemIdx(dist)
+		}
+		// for all clusters upd centroids
+		for i := 0; i < k; i++ {
+			for j := 0; j < n_features; j++ {
+				val := 0.0
+				pn := 0
+				for idx, elem := range km.labels {
+					if elem == i {
+						val += x_train.At(idx, j)
+						pn += 1
+					}
+				}
+				km.centroids.Set(i, j, val/float64(pn))
+			}
+		}
+		norm = normDense(km.centroids, prev_ctrs)
 	}
 	return nil
 }
 
-//TODO: Move to distance.go : Euclidean norm of Dense
-func norm_vec(a, b *mat.Dense) (dist float64) {
-	rn, _ := a.Dims()
-	for i := 0; i < rn; i++ {
-		dist += math.Pow(core.VecEuclidean(mat.VecDenseCopyOf(a.RowView(i)), mat.VecDenseCopyOf(b.RowView(i))), 2)
+func (km *KMNS) Predict(test core.Train) (labels []int) {
+	x_test := core.MakeMatrixFromTrain(test)
+	n_instance, _ := x_test.Dims()
+	k, _ := km.centroids.Dims()
+	for i := 0; i < n_instance; i++ {
+		dist := make([]float64, k)
+		for j := 0; j < k; j++ {
+			dist[j] = core.VecEuclidean(mat.VecDenseCopyOf(km.centroids.RowView(j)), mat.VecDenseCopyOf(x_test.RowView(i)))
+		}
+		labels = append(labels, minElemIdx(dist))
 	}
-	return math.Sqrt(dist)
+	return labels
 }
 
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+func normDense(a, b *mat.Dense) float64 {
+	rn, cn := a.Dims()
+	c := mat.NewDense(rn, cn, nil)
+	c.Sub(b, a)
+	return mat.Norm(c, 2)
+}
 
-// //Rewrite in terms of utils/vector
-// type Vec []float64
-
-// type ClusterObj struct {
-// 	ClusterNum int
-// 	Vec
-// }
-
-// type DistFunc func(vec1, vec2 []float64) (float64, error)
-
-// func L1(vec1, vec2 []float64) (float64, error) {
-// 	var dist float64
-// 	for i := range vec1 {
-// 		dist += math.Abs(vec1[i] - vec2[i])
-// 	}
-// 	return dist, nil
-// }
-
-// func L2(vec1, vec2 []float64) (float64, error) {
-// 	var dist float64
-// 	for i := range vec1 {
-// 		dist += (vec1[i] - vec2[i]) * (vec1[i] - vec2[i])
-// 	}
-// 	return math.Sqrt(dist), nil
-// }
-
-// func CanberraDistance(vec1, vec2 []float64) (float64, error) {
-// 	var dist float64
-// 	for i := range vec1 {
-// 		dist += (math.Abs(vec1[i]-vec2[i]) / (math.Abs(vec1[i]) + math.Abs(vec2[i])))
-// 	}
-// 	return dist, nil
-// }
-
-// func (vec Vec) Add(vec1 Vec) {
-// 	for i, val := range vec1 {
-// 		vec[i] += val
-// 	}
-// }
-
-// func (vec Vec) Mul(scal float64) {
-// 	for i := range vec {
-// 		vec[i] *= scal
-// 	}
-// }
-
-// func near(obj ClusterObj, centers []Vec, distFunc DistFunc) (int, float64) {
-// 	cluster_idx := 0
-// 	minDist, _ := distFunc(obj.Vec, centers[0])
-// 	for i := 1; i < len(centers); i++ {
-// 		dist, _ := distFunc(obj.Vec, centers[i])
-// 		if dist < minDist {
-// 			minDist = dist
-// 			cluster_idx = i
-// 		}
-// 	}
-
-// 	return cluster_idx, minDist
-// }
-
-// func seed(data []ClusterObj, k int, distFunc DistFunc) []Vec {
-// 	s := make([]Vec, k)
-// 	// rand.Seed(time.Now().UnixNano())
-// 	s[0] = data[rand.Intn(len(data))].Vec
-// 	d2 := make([]float64, len(data))
-// 	for i := 1; i < k; i++ {
-// 		var sum float64
-// 		for j, obj := range data {
-// 			_, d2[j] = near(obj, s[:i], distFunc)
-// 			sum += d2[j]
-// 		}
-// 		target := rand.Float64() * sum
-// 		var j int
-// 		for sum = d2[0]; sum < target; sum += d2[j] {
-// 			j++
-// 		}
-// 		s[i] = data[j].Vec
-// 	}
-// 	return s
-// }
-
-// func kmeans(data []ClusterObj, centers []Vec, distFunc DistFunc, threshold int) ([]ClusterObj, error) {
-// 	var counter int
-// 	for i, obj := range data {
-// 		nearestCluster, _ := near(obj, centers, distFunc)
-// 		data[i].ClusterNum = nearestCluster
-// 	}
-// 	cLen := make([]int, len(centers))
-// 	for n := len(data[0].Vec); ; {
-// 		for i := range centers {
-// 			centers[i] = make(Vec, n)
-// 			cLen[i] = 0
-// 		}
-// 		for _, obj := range data {
-// 			centers[obj.ClusterNum].Add(obj.Vec)
-// 			cLen[obj.ClusterNum]++
-// 		}
-// 		for i := range centers {
-// 			centers[i].Mul(1 / float64(cLen[i]))
-// 		}
-// 		var changed int
-// 		for i, obj := range data {
-// 			if nearestCluster, _ := near(obj, centers, distFunc); nearestCluster != obj.ClusterNum {
-// 				changed++
-// 				data[i].ClusterNum = nearestCluster
-// 			}
-// 		}
-// 		counter++
-// 		if counter == 0 || counter > threshold {
-// 			return data, nil
-// 		}
-// 	}
-// }
-
-// func Kmeans(rawData [][]float64, k int, distFunc DistFunc, threshold int) ([]int, error) {
-// 	data := make([]ClusterObj, len(rawData))
-// 	for i, v := range rawData {
-// 		data[i].Vec = v
-// 	}
-
-// 	seeds := seed(data, k, distFunc)
-// 	clustData, err := kmeans(data, seeds, distFunc, threshold)
-// 	lables := make([]int, len(clustData))
-// 	for i, obj := range clustData {
-// 		lables[i] = obj.ClusterNum
-// 	}
-// 	return lables, err
-// }
+func minElemIdx(d []float64) (idx int) {
+	min := d[0]
+	for i := 0; i < len(d); i++ {
+		if d[i] < min {
+			min = d[i]
+			idx = i
+		}
+	}
+	return idx
+}
--- unsupervisedlearning/clustering/kmeans/kmeans_test.go
+++ unsupervisedlearning/clustering/kmeans/kmeans_test.go
@@ -1,47 +1,24 @@
 package kmeans
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/bayesiangopher/bayesiangopher/core"
 )
 
-//TODO: Plot results for detailed check
 func TestKmeans(t *testing.T) {
-	r := core.CSVReader{Path: "../../../datasets/kmeans_iris.csv"}
-	train := r.Read(true)
+	// func BenchmarkKmeans(b *testing.B) {
+	r_train := core.CSVReader{Path: "train.csv"}
+	r_test := core.CSVReader{Path: "test.csv"}
+	// r := core.CSVReader{Path: "../../../datasets/the_iris_kmeans_dataset.csv"}
+	train := r_train.Read(true)
+	test := r_test.Read(true)
 
 	k := 3
-	kmns := KMNS{}
-	//Fit (data, k)
+	kmns := KMNS{state_init: false}
 	kmns.Fit(train, k)
-	// filePath, err := filepath.Abs("iris.csv")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// content, err := ioutil.ReadFile(filePath)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// lines := strings.Split(string(content), "\n")
-	// lines = lines[:len(lines)-1]
-	// irisData := make([][]float64, len(lines))
-	// irisLabels := make([]string, len(lines))
-	// for ii, line := range lines {
-	// 	vector := strings.Split(line, ",")
-	// 	label := vector[len(vector)-1]
-	// 	vector = vector[:len(vector)-1]
-	// 	floatVector := make([]float64, len(vector))
-	// 	for jj := range vector {
-	// 		floatVector[jj], err = strconv.ParseFloat(vector[jj], 64)
-	// 	}
-	// 	irisData[ii] = floatVector
-	// 	irisLabels[ii] = label
-	// }
-	// labels, err := Kmeans(irisData, 3, L1, 10)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println(labels)
+	fmt.Println(kmns.labels)
+	lbs := kmns.Predict(test)
+	fmt.Println(lbs)
 }
